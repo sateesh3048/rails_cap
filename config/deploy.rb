2869c2f4b754c02d773379f140e54d261e26626b
Sateesh Kambhampati <sateesh_kambhampati@gmail.com>
2017-12-07 16:47:35 +0530

    updating code

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,10 @@ set :repo_url, "https://github.com/sateesh3048/rails_cap"
 # Default deploy_to directory is /var/www/my_app_name
 server '127.0.0.1', user: "deployer", roles: %w{app web db}
 
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+set :config_files, %w{config/database.yml config/secrets.yml}
+
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
@@ -40,18 +44,52 @@ server '127.0.0.1', user: "deployer", roles: %w{app web db}
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 namespace :deploy do 
+
   desc "create database if not exists"
   task :create_db do 
     on roles(:db) do 
       execute :rake, "db:create"
     end
   end
+
+  desc "I am saying start it" 
+  task :say_start do 
+    on roles(:app) do
+      puts "GREATE <<<<<<< >>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+    end
+  end 
+
   desc "Make sure local git is in sync with remote."
   task :print_msg do 
     on roles(:app) do
       p "I am inside print_msg<<<<<<<<<<<888888888888888"
     end
   end
-#  before :starting, :print_msg
- # after :finishing, :clean_up
+
+  desc "checking myself"
+  task :check_it do
+    on roles(:app) do
+      p "Checkinglllllllllllllllllllllllll"
+    end
+  end
+
+  desc "running seed data"
+    task :run_seed  do 
+      on roles(:db) do 
+        p "I am seeding default data"
+      end
+    end
+
+  desc "Say by to every one"
+  task :say_by do
+    on roles(:app) do
+      p "Good Byeeeeeeeeeeeeeeeeeeee"
+    end
+  end
+
+  before :starting, :print_msg
+  after :print_msg, :say_start
+  after "deploy:migrate", :run_seed
+  after :finishing, :check_it
+  after :check_it, :say_by
 end
